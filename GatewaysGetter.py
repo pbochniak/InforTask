c063ceebb9138bddccdc85efb105fffef0a43cdf
Paweł Bochniak <pbochniak@gmail.pl>
2020-03-05 19:34:15 +0100

    finish task

--- GatewaysGetter.py
+++ GatewaysGetter.py
@@ -1,6 +1,8 @@
 import json
 import argparse
+
 import boto3
+from botocore.exceptions import BotoCoreError
 
 REST_METHODS = ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS", "HEAD"]
 FORMATS = ["json", "json-pretty", "csv"]
@@ -32,29 +34,60 @@ def create_args_parser():
         default=["ALL"],
         nargs="+",
         metavar="REST_METHOD",
+        choices=REST_METHODS,
         help="REST method(s) filter if not specified that means ALL",
     )
     return parser
 
 
+class Getter:
+    def __init__(self, config):
+        self.__config = config
+        self.__client = boto3.Session(
+            profile_name=self.__config.profile, region_name=self.__config.region
+        ).client("apigateway")
+        self.__apis = {}
+
+    def load(self):
+        for api in self.__client.get_rest_apis()["items"]:
+            self.load_resources(api)
+            self.__apis[api["name"]] = api
+
+    def load_resources(self, api):
+        endpoints = {}
+        for resource in self.__client.get_resources(restApiId=api["id"])["items"]:
+            self.load_methods(api, resource)
+            endpoints[resource["path"]] = resource
+        api["endpoints"] = endpoints
+
+    def load_methods(self, api, resource):
+        methods = resource["resourceMethods"]
+        for method in methods.keys():
+            if (
+                method not in self.__config.methods
+                and "ALL" not in self.__config.methods
+            ):
+                continue
+            methodInfo = self.__client.get_method(
+                restApiId=api["id"], resourceId=resource["id"], httpMethod=method
+            )
+            del methodInfo["ResponseMetadata"]
+            methods[method] = methodInfo
+
+    def output(self):
+        if self.__config.format == "json":
+            return json.dumps(self.__apis, default=str)
+        if self.__config.format == "json-pretty":
+            return json.dumps(self.__apis, indent=4, default=str)
+        if self.__config.format == "csv":
+            raise NotImplementedError("CSV format TODO")
+
+
 if __name__ == "__main__":
     args_parser = create_args_parser()
-    args = args_parser.parse_args()
-    print(args)
-    session = boto3.Session(profile_name=args.profile)
-    client = session.client("apigateway", region_name=args.region)
-    apis = client.get_rest_apis()["items"]
-    for api in apis:
-        resources = client.get_resources(restApiId=api["id"])["items"]
-        print(json.dumps(api, indent=4, default=str))
-        for index, resource in enumerate(resources):
-            for method in resource["resourceMethods"].keys():
-                if "ALL" in args.methods or method in args.methods:
-                    methodInfo = client.get_method(
-                        restApiId=api["id"],
-                        resourceId=resource["id"],
-                        httpMethod=method,
-                    )
-                    del methodInfo["ResponseMetadata"]
-                    resource["resourceMethods"][method] = methodInfo
-            print(index, json.dumps(resource, indent=4, default=str))
+    try:
+        getter = Getter(args_parser.parse_args())
+        getter.load()
+        print(getter.output())
+    except BotoCoreError as ex:
+        print(f"ERROR: {str(ex)}")
